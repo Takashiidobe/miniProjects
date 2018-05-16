be3ded00c31442b85da392e356e34c2c71e4790f
Takashi Idobe <idobetakashi@gmail.com>
2018-05-16 18:41:07 -0500

    a more generalized version allowing for user input

--- passwordGenerator/main.js
+++ passwordGenerator/main.js
@@ -1,8 +1,11 @@
 function passwordGenerator() {
-    let passwordLength = 8;
+    let passwordLength = parseInt(
+      prompt('How long would you like your password to be?', 20)
+      .trim()
+      .replace(/[^0-9]/gi, ''));
     let password = '';
     const charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < passwordLength; i++) {
       password += charSet.charAt(Math.floor(Math.random() * 62));
     }
     return password;
